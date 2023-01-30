c8954151c1d8ffc2a7227b9e9544a061f47c8d89
agreco <greco031@umn.edu>
2023-01-29 21:01:30 -0600

    Condition and labels modified

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,5 @@
 #Import and Labeling
-rt_df = read.csv("week2.csv", header = TRUE)
+rt_df <- read.csv("week2.csv", header = TRUE)
+rt_df$factor_condition <- factor(rt_df$condition, labels = c("Control", "Experimental"))
+rt_df$factor_gender <- factor(rt_df$gender, labels = c("Female", "Male", "Nonbinary"))
+  
